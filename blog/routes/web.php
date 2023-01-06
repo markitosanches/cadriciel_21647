8e62b901c94498c9cfc7f92e10030d04b8efffa4
Marcos <markitosanches@gmail.com>
2023-01-06 10:54:30 -0500

    Seance 2

---
+++ blog/routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello-test', function(){
+    return view('hello');
+});
+*/
+
+use App\Http\Controllers\BlogController;
+
+Route::get('/', [BlogController::class, 'index']);
+
+Route::get('/home', [BlogController::class, 'index']);
+
